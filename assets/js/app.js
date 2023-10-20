4fc088a19b7c9afcab1583856914e3606e5f2b07
shaheen <avrostu@gmail.com>
2023-10-20 23:51:09 +0600

    First Letter of Logo changed to Capital Letter!

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,9 +2,9 @@ const menuButton = document.querySelector("#menu-btn");
 const mobileMenu = document.querySelector("#mobile-menu");
 
 menuButton.addEventListener("click", function () {
-  if (mobileMenu.style.display == "none") {
-    mobileMenu.style.display = "block";
-  } else {
+  if (mobileMenu.style.display == "block") {
     mobileMenu.style.display = "none";
+  } else {
+    mobileMenu.style.display = "block";
   }
 });
